b970f5c0905037a8a6e4ad74cb31698bf53fc3b0
YONGHUNI <dydgns0556@gmail.com>
2024-10-11 18:24:43 +0900

    mod: aesthetics

--- quarto-version.lua
+++ quarto-version.lua
@@ -8,18 +8,17 @@ function getOS()
 	local fh,err = assert(io.popen("uname -o 2>/dev/null","r"))
 	if fh then
 		osname = fh:read()
-  -- Windows?
-  else
-    local handle = io.popen("ver")
-    local version = handle:read("*a")
-    handle:close()
 	end
+
+  local handle = io.popen("ver")
+  local version = handle:read("*a")
+  handle:close()
 
 	return osname or version
 end
 
 function RawInline (raw)
   if raw.format:match 'html' and raw.text == '<?quarto.version?>'then
-      return tostring(quarto.version).." compiled on "..getOS()
+      return tostring(quarto.version)..", compiled on "..getOS()
   end
 end

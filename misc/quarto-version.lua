6aaa9597739f513eea00fd9b9b732b6660f6120d
YONGHUNI <dydgns0556@gmail.com>
2024-10-12 02:19:09 +0900

    fix: giscus darkmode

---
+++ misc/quarto-version.lua
@@ -0,0 +1,24 @@
+function getOS()
+	-- ask LuaJIT first
+	if jit then
+		return jit.os
+	end
+
+	-- Unix, Linux variants
+	local fh,err = assert(io.popen("uname -o 2>/dev/null","r"))
+	if fh then
+		osname = fh:read()
+	end
+
+  local handle = io.popen("ver")
+  local version = handle:read("*a")
+  handle:close()
+
+	return osname or version
+end
+
+function RawInline (raw)
+  if raw.format:match 'html' and raw.text == '<?quarto.version?>'then
+      return tostring(quarto.version)..", compiled on "..getOS()
+  end
+end

444aa08f6648aa0cf8e5132b9a0279b638ff39bd
YONGHUNI <dydgns0556@gmail.com>
2024-10-18 18:55:35 +0900

    post(new): shinylive & quartolive

--- _extensions/quarto-ext/shinylive/shinylive.lua
+++ _extensions/quarto-ext/shinylive/shinylive.lua
@@ -70,7 +70,7 @@ end
 -- @param input: string to pipe into to r-shinylive
 function callRShinylive(args, input)
   args = { "-e",
-    "shinylive:::quarto_ext()",
+    "shinylive::quarto_ext()",
     table.unpack(args) }
 
   -- Try calling `pandoc.pipe('Rscript', ...)` and if it fails, print a message
